f208fa4ba88cf90d343b8ccfd4f5d50dda5d2123
Luka Jincharadze <lukajinchara@gmail.com>
2023-04-28 17:19:43 +0400

    done js functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 navbarLinks = document.getElementById("navbarLinks");
 togglebutton = document.getElementById("toggle-button");
+section = document.querySelector("section");
 
+function toggleNavbar() {
+  if (navbarLinks.classList.contains("active")) {
+    navbarLinks.classList.remove("active");
+    section.style.marginTop = "130px";
+  } else {
+    navbarLinks.classList.add("active");
+    section.style.marginTop = "0";
+  }
+}
+
+togglebutton.onclick = toggleNavbar;
